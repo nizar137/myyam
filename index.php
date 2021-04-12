7604ccdd79f467503dad4df779f8b9a13c0eb9fb
nizar137 <82389500+nizar137@users.noreply.github.com>
2021-04-12 13:38:31 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MYYAM</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+      <meta name="viewport" content="width=device-width, initial-scale=0.28">
+
+</head>
+<body>
+
+           <div class="a">
+	          
+                                  <div id="a1">
+                                  	    <p>
+                                  	          <img src="img/sig4.jpg", width="160">
+                                        </p>
+
+                                  </div>
+
+
+                                   <div id="a2">
+                                  	    <p>
+                                  	          <img src="img/ph1.jpg", width="500" height="200">
+                                        </p>
+
+                                  </div>
+
+
+           </div>
+
+
+             <div class="b1"> 
+                         <ul>
+                             <li>
+                               <a href="film/film1.php">FILMS</a>
+
+                             </li>
+              
+
+                         </ul>
+
+
+           </div>
+
+                        <div class="b2"> 
+                         <ul>
+                             <li>
+                               <a href="doc1/doc1.php">DOCUMENTAIRES  </a>
+
+                             </li>
+              
+
+                         </ul>
+
+
+           </div>
+
+
+             <div class="b3"> 
+                         <ul>
+                             <li>
+                               <a href="">Accéder à votre compte</a>
+                                     <ul>
+                                        <li class="b31">
+                                          <a href="../login1m/login2.php">FORFAIT 1 mois</a>
+
+                                        </li>
+
+                                        <li>
+                                             <a href="../login6m/login2.php">FORFAIT 6 mois</a>
+
+                                        </li>
+
+
+                                        <li>
+                                          <a href="../login12m/login2.php">FORFAIT 12 mois</a>
+
+                                        </li>
+                                 
+                                 
+
+
+                                      </ul>
+
+                             </li>
+              
+
+                         </ul>
+
+
+           </div>
+
+
+
+
+
+
+            <div class="c">
+
+
+	                              <ul>
+	                                    	<li>
+	             	                            	<a href="filmaction/film1.php">Films D'action</a>
+	                                     	</li>
+
+
+	                                      	<li>
+	                                           		<a href="filmsusp/film1.php">Films suspens</a>
+	                                    	</li>
+
+	                                    		<li>
+	                                           		<a href="filmpol/film1.php">Films Policier</a>
+	                                    	</li>
+
+                                          <li>
+                                                <a href="filmsci/film1.php"> Fiction</a>
+                                        </li>
+
+                                          <li>
+                                                <a href="filmara/film1.php">Films Arabes</a>
+                                        </li>
+
+
+
+
+
+
+	                              </ul>
+
+            </div>
+
+
+
+            <div class="d">
+	          	                  <ul>
+	                                    	<li>
+	             	                            	<a href="docastro/doc1.php">Astronomie</a>
+	                                     	</li>
+
+
+	                                      	<li>
+	                                           		<a href="docnat/doc1.php">Nature</a>
+	                                    	</li>
+
+	                                    		<li>
+	                                           		<a href="docani/doc1.php">Animaliers</a>
+	                                    	</li>
+
+
+                                          <li>
+                                                <a href="docsci/doc1.php">Sciences</a>
+                                        </li>
+
+
+                                          <li>
+                                                <a href="docpol/doc1.php"> Sociale</a>
+                                        </li>
+
+                                          <li>
+                                                <a href="docbat/doc1.php"> Architecture</a>
+                                        </li>
+
+
+
+
+
+
+
+	                              </ul>
+
+            </div>
+
+            <div class="e">
+
+                 <div>
+                
+                          <h3>
+                            Devenez myyam pro. Une gamme de films de hautes qualités et autant de documentaires de qualité hd
+
+                            </h3>
+
+                             <h3>
+                            la validité de votre offre sera par un émail. un code de validation vous sera transmis pour activer votre offre. pour plus d'information contacter nous sur toutscience@gmail.com.
+
+                            </h3>
+
+
+                             <h3>
+                            La modalité de paiement vous sera envoyé par émail une fois vous avez fixé la durée de votre offre.
+
+                            </h3>
+
+                             <h2>
+                            Email Myyam : toutscience@gmail.com
+
+                            </h2>
+
+
+                 </div>
+                 <div>
+                   <table>
+                              <tr>
+                                     <th>&nbsp;</th>
+                                      <th>Fofait 1</th>
+                                     <th>Fofait 2</th>
+                                      <th>Fofait 3</th>
+
+
+
+
+                               </tr>
+
+                               <tr>
+                                     <td>Durée</td>
+
+                                     <td>1 Mois</td>
+                                     <td>6 mois</td>
+                                     <td>12 mois</td>
+
+                               </tr>
+
+                               <tr>
+                                     <td>Montant</td>
+
+                                     <td>5 dinars</td>
+                                     <td>20 dinars</td>
+                                     <td>30 dinars</td>
+
+                               </tr>
+
+
+                               <tr>
+                                     <td>
+                                         &nbsp;
+
+
+                                     </td>
+                                      <td>
+                                              <ul>
+
+                                                         <li>
+                                                                  <a href="../login1m/register.php">Valider</a>
+                                                         </li>
+
+                                               </ul>
+
+                                      
+                                    </td>
+
+                                     <td>
+                                                <ul>
+
+                                                         <li>
+                                                                  <a href="../login6m/register.php">Valider</a>
+                                                         </li>
+
+                                               </ul>
+
+                                     </td>
+
+                                    </td>
+                                      <td>
+
+                                                 <ul>
+
+                                                         <li>
+                                                                  <a href="../login12m/register.php">Valider</a>
+                                                         </li>
+
+                                               </ul>
+
+                                    </td>
+
+
+
+     
+
+                               </tr>
+
+
+                   </table>
+
+                 </div>
+
+              
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+</body>
+</html>
