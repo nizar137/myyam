6dea3e5a166a8c75b46a7297cef1197b4d4773ee
nizar137 <82389500+nizar137@users.noreply.github.com>
2021-04-12 13:58:38 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MYYAM</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+      <meta name="viewport" content="width=device-width, initial-scale=0.28">
+
+</head>
+  <body>
+    hellow
+    </body>
+</html>
